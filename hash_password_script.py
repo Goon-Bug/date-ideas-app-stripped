faa6eb39381c3afd169359d639ad9e32c524c96b
Goon-Bug <goon-bug@hotmail.com>
2025-06-05 17:16:48 +0100

    Remove Dockerfile, docker-compose.yaml, and hash_password_script.py

--- hash_password_script.py
+++
@@ -1,8 +0,0 @@
-from flask_bcrypt import Bcrypt
-
-password = 'TestPassword1!!'
-bcrypt = Bcrypt()
-hashed_password = bcrypt.generate_password_hash(password)
-
-hashed_password = hashed_password.decode('utf-8')
-print(hashed_password)
