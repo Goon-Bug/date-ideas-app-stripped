7566afc0334e878e77c51cdd6d88fb4af5d524a5
Goon-Bug <goon-bug@hotmail.com>
2025-06-13 13:38:48 +0100

    Refactor setup_database function to accept a custom SQLite database name

--- backend/database/setup_dates_db.py
+++ backend/database/setup_dates_db.py
@@ -8,11 +8,11 @@ SQLITE_DB = "original_dates.db"
 logger = logging.getLogger(__name__)
 
 
-def setup_database(db_url=None):
+def setup_database(db_url=None, sqlite_db=SQLITE_DB):
     """Set up the SQLite database. If no db_url is provided, use the default file-based database."""
     if db_url is None:
-        db_url = f"sqlite:///{SQLITE_DB}"  # pragma: no cover
-    engine = create_engine(f'sqlite:///{SQLITE_DB}')
+        db_url = f"sqlite:///{sqlite_db}"  # pragma: no cover
+    engine = create_engine(f'sqlite:///{sqlite_db}')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     return Session()
