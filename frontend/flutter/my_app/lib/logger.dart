c04f2dbcda556941e28022a309dc3f9e8390f4a1
Goon-Bug <goon-bug@hotmail.com>
2025-06-24 13:09:33 +0100

    Add logger configuration with file logging support

---
+++ frontend/flutter/my_app/lib/logger.dart
@@ -0,0 +1,25 @@
+import 'package:date_spark_app/file_log_writer.dart';
+import 'package:flutter/foundation.dart';
+import 'package:logging/logging.dart';
+
+final Logger appLogger = Logger('DateSparkApp');
+final FileLogWriter fileLogWriter = FileLogWriter();
+
+final bool isProduction = bool.fromEnvironment('dart.vm.product');
+
+Future<void> configureLogger() async {
+  await fileLogWriter.init();
+
+  Logger.root.level = isProduction ? Level.WARNING : Level.ALL;
+
+  Logger.root.onRecord.listen((record) {
+    final logMessage =
+        '${record.level.name} | ${record.time.toIso8601String()} | ${record.loggerName}: ${record.message}';
+
+    if (kDebugMode) {
+      print(logMessage);
+    }
+
+    fileLogWriter.write(record);
+  });
+}
