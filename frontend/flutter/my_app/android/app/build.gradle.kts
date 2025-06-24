6036fba02b97e261e8a81e0b40acca291d73d19c
Goon-Bug <goon-bug@hotmail.com>
2025-06-24 13:08:04 +0100

    Remove unused product flavors from build.gradle.kts

--- frontend/flutter/my_app/android/app/build.gradle.kts
+++ frontend/flutter/my_app/android/app/build.gradle.kts
@@ -36,19 +36,6 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-
-    flavorDimensions += "default"
-    productFlavors {
-        create("dev") {
-            dimension = "default"
-            applicationIdSuffix = ".dev"
-            versionNameSuffix = "-dev"
-        }
-        create("prod") {
-            dimension = "default"
-            // No suffix for prod
-        }
-    }
 }
 
 flutter {
