b041701f3a867379549a484019056d0ecb5b7621
Goon-Bug <goon-bug@hotmail.com>
2025-06-25 12:27:26 +0100

    Remove unused userId field from TimelineItem class

--- frontend/flutter/my_app/lib/timeline/models/timeline.dart
+++ frontend/flutter/my_app/lib/timeline/models/timeline.dart
@@ -5,7 +5,6 @@ class TimelineItem {
   final String dateId;
   final String date;
   final String imagePath;
-  final String userId;
   final String? description;
   final String dateTitle;
 
@@ -14,7 +13,6 @@ class TimelineItem {
     required this.dateId,
     required this.date,
     required this.imagePath,
-    required this.userId,
     required this.dateTitle,
     this.description,
   });
@@ -26,7 +24,6 @@ class TimelineItem {
         'dateTitle': dateTitle,
         'imagePath': imagePath,
         'description': description,
-        'userId': userId,
       };
 
   factory TimelineItem.fromJson(Map<String, dynamic> json) => TimelineItem(
@@ -36,7 +33,6 @@ class TimelineItem {
         dateTitle: json['dateTitle'],
         imagePath: json['imagePath'],
         description: json['description'],
-        userId: json['userId'],
       );
 
   static List<TimelineItem> decodeList(String jsonString) {
@@ -65,7 +61,6 @@ class TimelineItem {
       date: date ?? this.date,
       dateTitle: dateTitle ?? this.dateTitle,
       imagePath: imagePath ?? this.imagePath,
-      userId: userId ?? this.userId,
       description: description ?? this.description,
     );
   }
