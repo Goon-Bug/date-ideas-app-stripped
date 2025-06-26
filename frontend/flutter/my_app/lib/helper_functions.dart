7857dfa0ad2302968d382620582164273bf1b1de
Goon-Bug <goon-bug@hotmail.com>
2025-06-26 10:41:28 +0100

    Update default storage values for token count and icon image

--- frontend/flutter/my_app/lib/helper_functions.dart
+++ frontend/flutter/my_app/lib/helper_functions.dart
@@ -54,9 +54,9 @@ Future<void> addDefaultsToStorage() async {
   _log.fine('All storage data: $allStorageData');
   if (allStorageData.isEmpty) {
     _log.fine('Storage is empty, adding defaults');
-    await storage.write(key: 'tokenCount', value: '1');
+    await storage.write(key: 'tokenCount', value: '100');
     await storage.write(
-        key: 'iconImage', value: 'assets/profile_icons/icon_0.png');
+        key: 'iconImage', value: 'assets/profile_icons/icon_1.png');
   } else {
     _log.fine('Storage already has data, skipping defaults');
   }
